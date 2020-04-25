9817eb7b1ddfc3cf1d10f49a7d23b1a04b027407
adele-angel <adeler.ang@gmail.com>
2020-04-26 00:14:21 +0300

    Updates

---
+++ precache-manifest.44acaf2e34b9a60e36b4b8a2209f52fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a41e804286e0fc210f5ca1e2228bb1aa",
+    "url": "/react-mobx-study/index.html"
+  },
+  {
+    "revision": "e04b4f607b0fb1b911a9",
+    "url": "/react-mobx-study/static/css/main.440b6247.chunk.css"
+  },
+  {
+    "revision": "15caa12959bade9d6bc7",
+    "url": "/react-mobx-study/static/js/2.23880347.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/react-mobx-study/static/js/2.23880347.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e04b4f607b0fb1b911a9",
+    "url": "/react-mobx-study/static/js/main.2171f06d.chunk.js"
+  },
+  {
+    "revision": "97cd75915bfb75e03b41",
+    "url": "/react-mobx-study/static/js/runtime-main.809f918a.js"
+  },
+  {
+    "revision": "c31c541e58e6c4f5bc9251983730c744",
+    "url": "/react-mobx-study/static/media/logo.c31c541e.svg"
+  }
+]);
